e1c5aa11f953851742fcd9a1e31770f341f7d19d
John Detter <john@detter.com>
2014-12-06 20:27:23 -0600

    Multiple player integration

--- src/battleShip/BattleShip.java
+++ src/battleShip/BattleShip.java
@@ -35,14 +35,14 @@ public class BattleShip
 	public static final int boat = 2;
 	public static final int submarine = 3;
 	public static final int destroyer = 3;
-	
+
 	// (1) Battleship, (2) Aircraft Carrier, (3) Boat, (4) Submarine, (5) Destroyer
 	public static final int BATTLESHIP = 0;
 	public static final int AIRCRAFT_CARRIER = 1;
 	public static final int BOAT = 2;
 	public static final int SUBMARINE = 3;
 	public static final int DESTROYER = 4;
-	
+
 	public static final int[] lengths = new int[]{4, 5, 2, 3, 3};
 
 	static class Player
@@ -111,7 +111,7 @@ public class BattleShip
 				int piece, boolean vertical)
 		{
 			int pieceLength = lengths[piece];
-			
+
 			boolean success = true;
 			int row = startRow;
 			int col = startCol;
@@ -141,9 +141,163 @@ public class BattleShip
 			return success;
 		}
 
+		public void promptForName(int player)
+		{
+			window.print("Player " + player + " name: ");
+			name = window.nextLine();
+		}
+		
+		public void setupBoard()
+		{
+			// lets print the blank board:
+
+			String question1 = "";
+			String question2 = "";
+
+			boolean shipBattle = true;
+			boolean shipAir = true;
+			boolean shipBoat = true;
+			boolean shipSub = true;
+			boolean shipDest = true;
+
+			String word1 = "(1) Battleship, ";
+			String word2 = "(2) Aircraft Carrier, ";
+			String word3 = "(3) Boat, ";
+			String word4 = "(4) Submarine, ";
+			String word5 = "(5) Destroyer";
+
+			while((shipBattle==true||shipAir==true||shipBoat==true||shipSub==true||shipDest==true))
+			{
+				window.println("Player " + name + " blank board:");
+				printboard();
+				window.println("Which ship would you like to place? Options: \n"
+						+ word1 + word2 + word3  + word4 + word5);
+				int shipType = window.nextInt() - 1;
+				switch(shipType)
+				{
+				case BATTLESHIP:
+					if(shipBattle==true)
+					{
+						question1 = "Which row would you like to place your battleship in?";
+						question2 = "Which column would you like to place your battleship in?"; 
+					}else{
+						window.println("You have already placed your battleship. \n"
+								+ "Please select a different ship to place.");
+						continue;
+					}
+					break;
+				case AIRCRAFT_CARRIER:
+					if(shipAir==true)
+					{
+						question1 = "Which row would you like to place your aircraft carrier in?";
+						question2 = "Which column would you like to place your aircraft carrier in?";
+					}else{
+						window.println("You have already placed your aircaft carrier. \n"
+								+ "Please select a different ship to place.");
+						continue;
+					}
+					break;
+				case BOAT:
+					if(shipBoat==true)
+					{
+						question1 = "Which row would you like to place your boat in?";
+						question2 = "Which column would you like to place your boat in?";
+					}else{
+						window.println("You have already placed your boat. \n"
+								+ "Please select a different ship to place");
+						continue;
+					}
+					break;
+				case SUBMARINE:
+					if(shipSub==true)
+					{
+						question1 = "Which row would you like to place your submarine in?";
+						question2 = "Which column would you like to place your submarine in?";
+					}else{
+						window.println("You have already placed your submarine. \n"
+								+ "Please select a different ship to place");
+						continue;
+					}
+					break;
+				case DESTROYER:
+					if(shipDest==true)
+					{
+						question1 = "Which row would you like to place your destroyer in?";
+						question2 = "Which column would you like to place your destroyer in?";
+					}else{
+						window.println("You have already placed your destroyer. \n"
+								+ "Please select a different ship to place");
+						continue;
+					}
+					break;
+				default:
+					if(!(shipType>=1||shipType<=5))
+					{
+						window.println("Invalid ship selection. Please enter a number 1-5.");
+					}
+				}
+
+				window.println(question1);
+				int row=window.nextInt();
+				int column=0;
+				boolean vertical=false;
+				if(row>=1||row<=10)
+				{
+					window.println(question2);
+					column =window.nextInt();
+					if(column>=1||column<=10)
+					{
+						window.println("Would you like the piece to be vertical? (Y/n)");
+						String orientation=window.nextLine();
+						if(orientation.equals("Y")||orientation.equals("y"))
+						{
+							vertical=true;
+						}else if(orientation.equals("N")||orientation.equals("n"))
+						{
+							vertical=false;
+						}else{
+							window.println("Invalid input. Please enter Y/y or N/n");
+							continue;
+						}
+					}else{
+						window.println("Invalid column number. Please choose column between 1-10");
+					}
+				}else{
+					window.println("Invalid row number. Please choose row between 1-10.");
+				}
+
+				switch(shipType)
+				{
+				case BATTLESHIP:
+					shipBattle = false;
+					word1 = "";
+					break;
+				case AIRCRAFT_CARRIER:
+					shipAir =  false;
+					word2 = "";
+					break;
+				case BOAT:
+					shipBoat = false;
+					word3 = "";
+					break;
+				case SUBMARINE:
+					shipSub = false;
+					word4 = "";
+					break;
+				case DESTROYER:
+					shipDest = false;
+					word5 = "";
+					break;
+				}
+				simplePlacePiece(row, column, shipType, vertical);
+				printboard();
+			}
+		}
+
 		private int boardRows;
 		private int boardColumns;
 		public String[][] board; // our board for our player
+		private String name;
 	}
 
 	public static Window window;
@@ -155,172 +309,16 @@ public class BattleShip
 
 		// setup player1 so that their board is empty
 		Player player1 = new Player();
+		player1.promptForName(1);
 		player1.initilizeBoard(10, 10);
+		player1.setupBoard();
 
 		// setup player2 so that their board is empty
 		Player player2 = new Player();
+		player2.promptForName(2);
 		player2.initilizeBoard(10, 10);
+		player2.setupBoard();
 
 
-		// lets print the blank board:
-		
-		String question1 = "";
-		String question2 = "";
-		boolean shipBattle = true;
-		boolean shipAir = true;
-		boolean shipBoat = true;
-		boolean shipSub = true;
-		boolean shipDest = true;
-		int playerTurn = 1;
-		String word1 = "(1) Battleship, ";
-		String word2 = "(2) Aircraft Carrier, ";
-		String word3 = "(3) Boat, ";
-		String word4 = "(4) Submarine, ";
-		String word5 = "(5) Destroyer";
-		while((shipBattle==true||shipAir==true||shipBoat==true||shipSub==true||shipDest==true)&playerTurn<=2)
-		{
-			if(playerTurn==1)
-			{
-			window.println("Player " + playerTurn + " blank board:");
-			player1.printboard();
-			}else if(playerTurn==2){
-				window.println("Player " + playerTurn + " blank board:");
-				player2.printboard();
-			}
-			window.println("Which ship would you like to place? Options: \n"
-					+ word1 + word2 + word3  + word4 + word5);
-			int shipType = window.nextInt() - 1;
-		switch(shipType)
-		{
-		case BATTLESHIP:
-			if(shipBattle==true)
-			{
-			question1 = "Which row would you like to place your battleship in?";
-			question2 = "Which column would you like to place your battleship in?"; 
-			}else{
-				window.println("You have already placed your battleship. \n"
-						+ "Please select a different ship to place.");
-				continue;
-			}
-			break;
-		case AIRCRAFT_CARRIER:
-			if(shipAir==true)
-			{
-			question1 = "Which row would you like to place your aircraft carrier in?";
-			question2 = "Which column would you like to place your aircraft carrier in?";
-			}else{
-				window.println("You have already placed your aircaft carrier. \n"
-						+ "Please select a different ship to place.");
-				continue;
-			}
-			break;
-		case BOAT:
-			if(shipBoat==true)
-			{
-			question1 = "Which row would you like to place your boat in?";
-			question2 = "Which column would you like to place your boat in?";
-			}else{
-				window.println("You have already placed your boat. \n"
-						+ "Please select a different ship to place");
-				continue;
-			}
-			break;
-		case SUBMARINE:
-			if(shipSub==true)
-			{
-			question1 = "Which row would you like to place your submarine in?";
-			question2 = "Which column would you like to place your submarine in?";
-			}else{
-				window.println("You have already placed your submarine. \n"
-						+ "Please select a different ship to place");
-				continue;
-			}
-			break;
-		case DESTROYER:
-			if(shipDest==true)
-			{
-			question1 = "Which row would you like to place your destroyer in?";
-			question2 = "Which column would you like to place your destroyer in?";
-			}else{
-				window.println("You have already placed your destroyer. \n"
-						+ "Please select a different ship to place");
-				continue;
-			}
-			break;
-		default:
-			if(!(shipType>=1||shipType<=5))
-			{
-			window.println("Invalid ship selection. Please enter a number 1-5.");
-			}
-		}
-
-		window.println(question1);
-		int row=window.nextInt();
-		int column=0;
-		boolean vertical=false;
-		if(row>=1||row<=10)
-		{
-			window.println(question2);
-			column =window.nextInt();
-			if(column>=1||column<=10)
-			{
-				window.println("Would you like the piece to be vertical? (Y/n)");
-				String orientation=window.nextLine();
-				if(orientation.equals("Y")||orientation.equals("y"))
-				{
-					vertical=true;
-				}else if(orientation.equals("N")||orientation.equals("n"))
-				{
-					vertical=false;
-				}else{
-					window.println("Invalid input. Please enter Y/y or N/n");
-					continue;
-				}
-			}else{
-				window.println("Invalid column number. Please choose column between 1-10");
-			}
-		}else{
-			window.println("Invalid row number. Please choose row between 1-10.");
-		}
-	
-		switch(shipType)
-		{
-		case BATTLESHIP:
-			shipBattle = false;
-			word1 = "";
-			break;
-		case AIRCRAFT_CARRIER:
-			shipAir =  false;
-			word2 = "";
-			break;
-		case BOAT:
-			shipBoat = false;
-			word3 = "";
-			break;
-		case SUBMARINE:
-			shipSub = false;
-			word4 = "";
-			break;
-		case DESTROYER:
-			shipDest = false;
-			word5 = "";
-			break;
-		}
-		if(playerTurn==1)
-		{
-			player1.simplePlacePiece(row, column, shipType, vertical);
-			player1.printboard();
-		}else if(playerTurn==2)
-		{
-			player2.simplePlacePiece(row, column, shipType, vertical);
-			player2.printboard();
-		}
-		if(shipBattle==false&shipAir==false&shipBoat==false&shipSub==false&shipDest==false)
-		{
-		playerTurn++;
-		}
-		
-		}
-		
 	}
 }
